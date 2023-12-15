469cc3ff06c73f0a762635b44085fce3b99bea5e
Seth Baldwin <sethnbaldwin@gmail.com>
2023-12-14 23:10:12 -0500

    Advent of code day 2 part 1 complete

--- Day 2/main.py
+++ Day 2/main.py
@@ -6,12 +6,50 @@ cube_count = {"red": 12,
 def cubeConundrum():
     with open('input', 'r') as file:
         # Read file line by line
+        game_number = 1
+        answer = 0
         for line in file:
-            print(line)
+            game = line.split(':')
+            sets = game[1].split(';')
+            for set in sets:
+                set_result = getSetResults(set)
+                if not isPossible(set_result):
+                    break
+                if set == sets[-1]:
+                    # if we make it to the last set and all previous sets were possible we can add this game to the answer sum.
+                    answer += game_number
+
+            game_number += 1
+        
+        return answer
+    
+# Will take in one "set". 1 of 3 in a game line
+# input: game set. "1 blue, 2 green, 3 red"
+# returns: Dict of set results. {'blue': 1, 'green': 2, 'red': 3}
+def getSetResults(set):
+    # a set should come in the form: 1 blue, 2 green, 3 red
+    sets = set.split(',')
+    results = {}
+    for i in range(len(sets)):
+        cubes = sets[i].strip()
+        cubes = cubes.split(' ')
+        results[cubes[1]] = int(cubes[0])
+    
+    return results
+
+# input: {'blue': 1, 'green': 2, 'red': 3}
+# returns: Bool
+def isPossible(set_results):
+    for item in set_results:
+        if item in cube_count:
+            if set_results[item] > cube_count[item]:
+                return False
+    return True
 
 
+print(cubeConundrum())
 
-# cubeConundrum()
+### Cube Conundrum
 
 ## Part 1 Store data
 # Lets think about how to organize our data
@@ -29,19 +67,5 @@ def cubeConundrum():
 # : ; ;
 # We can easily use these to separate each round
 # How do we get the number and color value from each group
-
-def getSetResults(set):
-    # a set should come in the form: 1 blue, 2 green, 3 red
-    sets = set.split(',')
-    results = []
-    for i in range(len(sets)):
-        cubes = sets[i].strip()
-        results.append(cubes.split(' '))
-    
-    return results
-
-#print(getSetResults(" 1 blue, 2 green, 3 red"))
-print("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".split(':'))
-
 
 ## Part 3 Look at the data and determine impossible games
